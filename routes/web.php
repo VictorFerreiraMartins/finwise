97d12dcaa6aaf7bc1e3157fd67a79cb18476abe4
Victor Martins <victorfm@live.com>
2025-10-10 17:01:18 -0300

    feat(routes): protect dashboard with auth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+
+Route::middleware('guest')->group(function (): void {
+    Route::get('/login', [LoginController::class, 'create'])->name('login');
+    Route::post('/login', [LoginController::class, 'store']);
+});
+
+Route::middleware('auth')->group(function (): void {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+    Route::post('/logout', [LoginController::class, 'destroy'])->name('logout');
 });

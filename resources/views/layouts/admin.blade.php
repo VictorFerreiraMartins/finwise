e3e938d7491363087f97795db89f777ac46c4779
Victor Martins <victorfm@live.com>
2025-10-10 16:36:12 -0300

    feat(view): scaffold adminlte base layout

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title', config('app.name', 'Laravel'))</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @stack('styles')
+</head>
+
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav ml-auto">
+                @stack('navbar-right')
+            </ul>
+        </nav>
+
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <a href="{{ url('/') }}" class="brand-link">
+                <span class="brand-text font-weight-light">{{ config('app.name', 'Laravel') }}</span>
+            </a>
+
+            <div class="sidebar">
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                        data-accordion="false">
+                        @stack('sidebar')
+                    </ul>
+                </nav>
+            </div>
+        </aside>
+
+        <div class="content-wrapper">
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            @hasSection('page_header')
+                                @yield('page_header')
+                            @else
+                                <h1 class="m-0">@yield('title', config('app.name', 'Laravel'))</h1>
+                            @endif
+                        </div>
+                        <div class="col-sm-6">
+                            @hasSection('breadcrumb')
+                                <ol class="breadcrumb float-sm-right">
+                                    @yield('breadcrumb')
+                                </ol>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <section class="content">
+                <div class="container-fluid">
+                    @yield('content')
+                </div>
+            </section>
+        </div>
+
+        <footer class="main-footer">
+            <div class="float-right d-none d-sm-inline">
+                @yield('footer-right')
+            </div>
+            <strong>&copy; {{ now()->year }} {{ config('app.name', 'Laravel') }}.</strong> All rights reserved.
+        </footer>
+    </div>
+
+    @stack('scripts')
+</body>
+
+</html>
